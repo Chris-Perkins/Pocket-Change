5886d0aef0821ff2e7c0188c720bf2186e60798b
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2019-03-01 23:34:30 -0500

    add gitignore, Podfile, Project files

---
+++ Lincoln Laws/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lincoln Laws
+//
+//  Created by Christopher Perkins on 3/1/19.
+//  Copyright © 2019 Christopher Perkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
